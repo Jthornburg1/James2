b0b3a537fcbc177173d09731841c89d2994af45f
jonathan thornburg <reliable2192@gmail.com>
2017-01-14 14:30:23 -0600

    has ui

---
+++ James2/SongCell.swift
@@ -0,0 +1,15 @@
+//
+//  SongCell.swift
+//  James2
+//
+//  Created by jonathan thornburg on 1/14/17.
+//  Copyright © 2017 jonathan thornburg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SongCell: UITableViewCell {
+    
+    
+}
--- James2/ViewController.swift
+++ James2/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITextFieldDelegate, UITableViewDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? SongCell
+        return cell!cq  Vxy eb  sycqe
+    }
 }
 
